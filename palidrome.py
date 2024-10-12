d4982f548ef649ba44698bb3bc5721b72341684b
Prajwal Gopal Poojary <prajwalgpy@gmail.com>
2024-10-12 19:13:25 +0530

    Dsa

--- palidrome.py
+++ palidrome.py
@@ -1,9 +1,12 @@
-lend = int(input("How many fibonacci yo whant"))
-start = 0
-second =1
-print(start,second)
-for i in range (lend):
-    newfib = second + start
-    print(newfib)
-    start= second
-    second = newfib
+num =int(input("Enerte the how many numbers you whant"))
+
+first = 0
+second = 1
+print(first)
+print(second)
+for i in range(num):
+    newPali = first + second
+    print(newPali)
+    first =  second
+    second = newPali
+    
